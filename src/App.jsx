6a9ff3878fe84c4cb700237a79230f7e001c0dae
vanjerson <vanjerson2@gmail.com>
2023-02-07 16:35:46 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import './index.css';
+import './css/style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
 	
-	const [items, setItems] = useState([
-		
-	]);
-
+	const [items, setItems] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
 	const [inputValue, setInputValue] = useState('');
 	const [totalItemCount, setTotalItemCount] = useState(0);
 
 	const handleAddButtonClick = () => {
-		const newItem = {
-			itemName: inputValue,
-			quantity: 1,
-			isSelected: false,
-		};
+
+    if (inputValue == "") {
+      setShowAlert(true);
+  } else {
+      const newItem = {
+          id: Math.floor(Math.random() * 1000),
+          itemName: inputValue,
+          quantity: 1,
+          isSelected: false,
+      };
 
 		const newItems = [...items, newItem];
 
 		setItems(newItems);
 		setInputValue('');
-		calculateTotal();
-	};
+		setTotalItemCount(totalItemCount + 1);
+	}
+};
+
+  const closeAlert = () => {
+    setShowAlert(false);
+}
 
 	const handleQuantityIncrease = (index) => {
 		const newItems = [...items];
@@ -38,11 +47,22 @@ const App = () => {
 	const handleQuantityDecrease = (index) => {
 		const newItems = [...items];
 
-		newItems[index].quantity--;
+		if(newItems[index].quantity <= 1) {
+      newItems[index].quantity = 1;
+  } else {
+    newItems[index].quantity--;
+  }
 
 		setItems(newItems);
 		calculateTotal();
 	};
+
+  const deleteItem = (index) => {
+    const newItems = [...items];
+    const deletedItem = newItems.splice(index,1)[0];
+    setItems(newItems);
+    setTotalItemCount((prevCount) => prevCount - deletedItem.quantity);
+  };
 
 
 	const toggleComplete = (index) => {
@@ -62,10 +82,25 @@ const App = () => {
 	};
 
 	return (
+
+    <div className='App'>
+
+
+
 		<div className='app-background'>
+
+    {showAlert && (
+    <div type="button" className="btn-close" data-bs-dismiss="alert" data-bs-target="#my-alert" aria-label="Close">
+        <button onClick={() => closeAlert()} type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <strong>Oh snap!</strong> Write something and try adding again.
+    </div>
+  )}
+
 			<div className='main-container'>
+        <h2>Black Market</h2>
 				<div className='add-item-box'>
 					<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
+          
 					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
 				</div>
 				<div className='item-list'>
@@ -92,6 +127,9 @@ const App = () => {
 								<button>
 									<FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease(index)} />
 								</button>
+                <button>
+									<FontAwesomeIcon icon={faTrashCan} onClick={() => deleteItem(index)} />
+								</button>
 							</div>
 						</div>
 					))}
@@ -100,6 +138,7 @@ const App = () => {
 				<div className='total'>Total: {totalItemCount}</div>
 			</div>
 		</div>
+  </div>
 	);
 };
 
